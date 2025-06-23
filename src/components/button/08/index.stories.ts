4392c459e853a6b92362e527c39e0487f4b7e342
zakzakst <e1433104@gmai.com>
2025-06-24 01:48:01 +0900

    修正

---
+++ src/components/button/08/index.stories.ts
@@ -0,0 +1,23 @@
+// https://quantitative.jp/
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Button08 } from ".";
+
+const meta = {
+  title: "Button/08",
+  component: Button08,
+  argTypes: {
+    children: { control: "text" },
+  },
+  args: {
+    href: "#",
+  },
+} satisfies Meta<typeof Button08>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "ボタン08",
+  },
+};
