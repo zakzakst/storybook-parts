2987ec657b7b859c5bfe41322fd47a8aa61e2a15
zakzakst <e1433104@gmai.com>
2025-06-26 01:06:02 +0900

    修正

---
+++ src/components/button/23/index.stories.tsx
@@ -0,0 +1,23 @@
+// https://leverages.jp/
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Button23 } from ".";
+
+const meta = {
+  title: "Button/23",
+  component: Button23,
+  argTypes: {
+    children: { control: "text" },
+  },
+  args: {
+    href: "#",
+  },
+} satisfies Meta<typeof Button23>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "BUTTON23",
+  },
+};
