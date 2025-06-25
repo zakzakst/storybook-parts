761217d68b997a43c33b36f9178731f541d62cc5
zakzakst <e1433104@gmai.com>
2025-06-25 11:38:00 +0900

    修正

--- MEMO.md
+++ MEMO.md
@@ -1,6 +1,6 @@
 ### 次やる
 
-https://zakzakst.github.io/parts/components/detail/button14.html
+https://zakzakst.github.io/parts/components/detail/button15.html
 
 ### やること
 
---
+++ src/components/button/13/index.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type Props = React.ComponentProps<"a">;
+
+export const Button13 = ({ className, children, ...rest }: Props) => {
+  return (
+    <a className={clsx(styles.button, className)} {...rest}>
+      <span className={styles.text}>{children}</span>
+      <span className={styles.icon} />
+    </a>
+  );
+};
