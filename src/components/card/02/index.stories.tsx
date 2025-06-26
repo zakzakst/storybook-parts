dd63363cbb7613d32995bdf23f68ebe3de480837
zakzakst <e1433104@gmai.com>
2025-06-27 07:59:34 +0900

    修正

---
+++ src/components/card/02/index.stories.tsx
@@ -0,0 +1,28 @@
+// https://www.teotoriatte.info/
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Card02 } from ".";
+
+const meta = {
+  title: "Card/02",
+  component: Card02,
+  argTypes: {
+    tag: { control: "text" },
+    heading: { control: "text" },
+  },
+  args: {
+    href: "#",
+  },
+} satisfies Meta<typeof Card02>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    tag: "タグが入ります",
+    icon: "http://placehold.jp/350x350.png?text=ICON",
+    heading: "見出しが入ります",
+    content:
+      "テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。",
+  },
+};
