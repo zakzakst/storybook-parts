ae22d58b9aacc28f690f9b1a0703883ec62f8dfb
zakzakst <e1433104@gmai.com>
2025-06-26 21:55:24 +0900

    修正

---
+++ src/components/button/39/index.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type Props = React.ComponentProps<"a">;
+
+export const Button39 = ({ className, children, ...rest }: Props) => {
+  return (
+    <a className={clsx(styles.button, className)} {...rest}>
+      <span className={styles.inner}>
+        {children}
+        <span className={styles.arrow} />
+      </span>
+    </a>
+  );
+};
