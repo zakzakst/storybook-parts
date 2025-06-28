b98191fae7c215bd6dff22e82ed1b5d1a8e6f563
zakzakst <e1433104@gmai.com>
2025-06-28 16:51:09 +0900

    修正

---
+++ src/components/card/08/index.stories.tsx
@@ -0,0 +1,26 @@
+// https://www.dot-st.com/cp/jeanasis/jeanasis_media
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Card08 } from ".";
+
+const meta = {
+  title: "Card/08",
+  component: Card08,
+  argTypes: {
+    date: { control: "text" },
+    heading: { control: "text" },
+  },
+  args: {
+    href: "#",
+  },
+} satisfies Meta<typeof Card08>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    thumbnail: "https://picsum.photos/400/200",
+    date: "2021.01.01",
+    heading: "CARD08",
+  },
+};
