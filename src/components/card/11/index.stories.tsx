083db06450d4a39871b97c3ff0c6890510ef6c1b
zakzakst <e1433104@gmai.com>
2025-06-29 17:53:08 +0900

    修正

---
+++ src/components/card/11/index.stories.tsx
@@ -0,0 +1,28 @@
+// https://umenaka.oiwai.online/
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Card11 } from ".";
+
+const meta = {
+  title: "Card/11",
+  component: Card11,
+  argTypes: {
+    heading: { control: "text" },
+    date: { control: "text" },
+  },
+  args: {
+    href: "#",
+  },
+} satisfies Meta<typeof Card11>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    heading: "カード11",
+    thumbnail: "https://picsum.photos/400/200",
+    content:
+      "テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。",
+    date: "2021.01.01",
+  },
+};
