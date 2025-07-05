970159c0c49c6ac7da3ceebec148f70a43a4ec52
zakzakst <e1433104@gmai.com>
2025-07-05 16:47:46 +0900

    修正

---
+++ src/components/heading/07/index.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type Props = Omit<React.ComponentProps<"h3">, "children">;
+
+export const Heading07 = ({ className, ...rest }: Props) => {
+  return (
+    <h3 className={clsx(styles.module, className)} {...rest}>
+      <span className={styles.num}>01</span>見出しが
+      <em>入ります</em>
+    </h3>
+  );
+};
