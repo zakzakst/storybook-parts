b25f32cd276fd468cb5823be23a2fe1c6b7a1204
zakzakst <e1433104@gmai.com>
2025-07-29 07:42:59 +0900

    修正

---
+++ src/components/scrollAnim/05/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+
+import { createScope, animate, onScroll } from "animejs";
+import clsx from "clsx";
+
+import styles from "./styles.module.css";
+
+import type { Scope } from "animejs";
+
+type OwnProps = {
+  text: string;
+};
+
+type Props = Omit<React.ComponentProps<"div">, "children"> & OwnProps;
+
+export const ScrollAnim05 = ({ className, text, ...rest }: Props) => {
+  const root = useRef(null);
+  const scope = useRef<Scope>(null);
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  useEffect(() => {
+    scope.current = createScope({ root }).add(() => {
+      animate(".animejs", {
+        autoplay: onScroll({
+          enter: "bottom-=50 top",
+          leave: "top+=50 bottom",
+          // debug: true,
+          onEnter: (self) => {
+            // console.log("onEnter");
+            setIsAnimated(true);
+            self.revert();
+          },
+        }),
+      });
+    });
+    return () => {
+      if (scope.current) {
+        scope.current.revert();
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={root}>
+      <div
+        className={clsx(
+          "animejs",
+          styles.module,
+          isAnimated && styles.isAnimated,
+          className
+        )}
+        {...rest}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
