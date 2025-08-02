f0b25d4b751e842934cc1a25000278237360ce05
zakzakst <e1433104@gmai.com>
2025-08-02 17:02:23 +0900

    fix

---
+++ src/hooks/useMousePosOnElement.ts
@@ -0,0 +1,72 @@
+import {
+  useState,
+  // useEffect,
+  useCallback,
+} from "react";
+
+type MousePos = {
+  x: number;
+  y: number;
+};
+
+const DefaultMousePos: MousePos = {
+  x: 0,
+  y: 0,
+};
+
+export const useMousePosOnElement = (
+  targetRef: React.RefObject<HTMLElement | null>
+) => {
+  const [pos, setPos] = useState<MousePos>(DefaultMousePos);
+
+  const updatePos = useCallback(
+    (e: React.MouseEvent) => {
+      if (!targetRef.current) return;
+      const clientRect = targetRef.current.getBoundingClientRect();
+      const pointerX = e.pageX - (clientRect.left + window.pageXOffset);
+      const pointerY = e.pageY - (clientRect.top + window.pageYOffset);
+      const x =
+        pointerX < 0
+          ? 0
+          : pointerX > targetRef.current.clientWidth
+          ? targetRef.current.clientWidth
+          : pointerX;
+      const y =
+        pointerY < 0
+          ? 0
+          : pointerY > targetRef.current.clientHeight
+          ? targetRef.current.clientHeight
+          : pointerY;
+      setPos({ x, y });
+    },
+    [targetRef]
+  );
+
+  const resetPos = useCallback(() => {
+    setPos(DefaultMousePos);
+  }, []);
+
+  // NOTE: マウス操作時に位置取得以外にも実行したい処理がある場合が多いため、下記の処理は利用しない判断（記述方法として覚えておきたいのでコメントアウトで残しておく）
+  // useEffect(() => {
+  //   const element = targetRef.current;
+  //   if (!element) return;
+
+  //   // イベントリスナーを追加
+  //   element.addEventListener("mouseenter", updatePos);
+  //   element.addEventListener("mousemove", updatePos);
+  //   element.addEventListener("mouseleave", updatePos);
+
+  //   // クリーンアップ関数でイベントリスナーを削除
+  //   return () => {
+  //     element.removeEventListener("mouseenter", updatePos);
+  //     element.removeEventListener("mousemove", updatePos);
+  //     element.removeEventListener("mouseleave", updatePos);
+  //   };
+  // }, [targetRef, updatePos]);
+
+  return {
+    pos,
+    updatePos, // 手動で呼び出したい場合のために残しておく
+    resetPos, // 手動でリセットしたい場合のために追加
+  };
+};
