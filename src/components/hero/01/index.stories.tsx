faa4fb3329327dfc62a07506c454c342dea27359
zakzakst <e1433104@gmai.com>
2025-08-23 16:53:56 +0900

    fix

--- src/components/hero/01/index.stories.tsx
+++ src/components/hero/01/index.stories.tsx
@@ -7,9 +7,12 @@ const meta = {
   title: "Hero/01",
   component: Hero01,
   argTypes: {
-    text: { control: "text" },
+    title1: { control: "text" },
+    title2: { control: "text" },
+    subTitle1: { control: "text" },
+    subTitle2: { control: "text" },
+    thumbnail: { control: "text" },
   },
-  // args: {},
 } satisfies Meta<typeof Hero01>;
 
 export default meta;
@@ -17,6 +20,10 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    text: "テキスト",
+    title1: "サブタイトル1行目",
+    title2: "サブタイトル2行目",
+    subTitle1: "MAIN TITLE 01",
+    subTitle2: "MAIN TITLE 02 長い文字列長い文字列",
+    thumbnail: "https://picsum.photos/id/100/1280/1280",
   },
 };
