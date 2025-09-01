6830177021d5c0913d80f56d62482445275aec44
zakzakst <e1433104@gmai.com>
2025-09-02 08:01:07 +0900

    fix

--- src/components/scrollAnim/16/index.tsx
+++ src/components/scrollAnim/16/index.tsx
@@ -1,18 +1,58 @@
-// https://zakzakst.github.io/parts/components/detail/scroll-anim17.html
+import { useEffect, useRef, useState } from "react";
+
 import clsx from "clsx";
 
 import styles from "./styles.module.css";
 
-type OwnProps = {
-  text: string;
-};
+type Props = Omit<React.ComponentProps<"ul">, "children">;
 
-type Props = Omit<React.ComponentProps<"div">, "children"> & OwnProps;
+const HidePos = 72;
 
-export const ScrollAnim16 = ({ className, text, ...rest }: Props) => {
+export const ScrollAnim16 = ({ className, ...rest }: Props) => {
+  const ticking = useRef(false);
+  const scrollPos = useRef(0);
+  const [isShow, setIsShow] = useState(true);
+
+  useEffect(() => {
+    const showAnim = () => {
+      if (!ticking.current) {
+        requestAnimationFrame(() => {
+          ticking.current = false;
+          const currentPos = window.scrollY;
+          if (currentPos < HidePos) {
+            // 表示領域内の場合
+            setIsShow(true);
+          } else if (currentPos < scrollPos.current) {
+            // 非表示領域内でスクロール方向が上の場合
+            setIsShow(true);
+          } else {
+            // 非表示領域内でスクロール方向が下の場合
+            setIsShow(false);
+          }
+          // スクロール量を更新
+          scrollPos.current = currentPos;
+        });
+        ticking.current = true;
+      }
+    };
+    window.addEventListener("scroll", showAnim, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", showAnim);
+    };
+  }, [setIsShow]);
+
   return (
-    <div className={clsx(styles.module, className)} {...rest}>
-      {text}
-    </div>
+    <ul
+      className={clsx(styles.module, isShow && styles.isShow, className)}
+      {...rest}
+    >
+      {Array(3)
+        .fill(null)
+        .map((_, index) => (
+          <li key={index}>
+            <a href="#">リンク{index}</a>
+          </li>
+        ))}
+    </ul>
   );
 };
